6789dca97b84a27488660de63b553a479ff6ad83
Akash Sharma <akashsharma110103@gmail.com>
2025-04-16 20:48:35 +0530

    updated

--- lambdaInterface.java
+++ lambdaInterface.java
@@ -7,7 +7,7 @@ interface MathOperation {
 public class lambdaInterface {
     public static void main(String[] args) {
         MathOperation add = (a, b) -> a + b;
-        MathOperation multiply = (a, b) -> a * b;
+        MathOperation multiply = (a, b) -> a * b; 
 
         System.out.println("Sum: " + add.operate(5, 3));        // Output: 8
         System.out.println("Product: " + multiply.operate(5, 3)); // Output: 15
